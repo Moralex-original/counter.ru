48436fb5a98cf087295f23f481a3c66055ce27a2
Moralex-original <124479386+Moralex-original@users.noreply.github.com>
2023-02-11 14:19:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,8 @@ function changeColor() {
   	// переменная для результата
     var result;
     // получаем первое и второе число
-    var number_1 = Number(document.getElementById("num1").value);
-    var number_2 = Number(document.getElementById("num2").value);
+    var number_1 = Number(document.getElementById("number_1").value);
+    var number_2 = Number(document.getElementById("number_2").value);
     // смотрим, что было в переменной с действием, и действуем исходя из этого
     switch (op) {
       case '+':
