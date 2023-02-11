d31aeacf4edba8a4f8600582bb4d531986db10b7
Moralex-original <124479386+Moralex-original@users.noreply.github.com>
2023-02-11 14:11:00 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 function changeColor() {
    document.getElementById("pip").style.color = "red";
 }
+
+// переменная, в которой хранится выбранное математическое действие
+  var op; 
+
+  // функция расчёта
+  function func() {
+  	// переменная для результата
+    var result;
+    // получаем первое и второе число
+    var number_1 = Number(document.getElementById("num1").value);
+    var number_2 = Number(document.getElementById("num2").value);
+    // смотрим, что было в переменной с действием, и действуем исходя из этого
+    switch (op) {
+      case '+':
+        result = number_1 + number_2;
+        break;
+      case '-':
+        result = number_1 - number_2;
+        break;
+      case '*':
+        result = number_1 * number_2;
+        break;
+      case '/':
+        result = number_1 / number_2;
+        break;
+    }
+
+  }
